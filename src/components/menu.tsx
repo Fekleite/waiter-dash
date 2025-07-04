1903b4aabca675cc8880355e282bf77c880f064c
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-03 23:04:09 -0300

    feat: add hover style on menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,59 +9,60 @@ import {
 
 export function MenuSidebar() {
   return (
-    <aside className="flex h-screen w-28 flex-col bg-gray-0">
+    <aside className="fixed top-0 bottom-0 left-0 z-10 flex h-screen w-28 flex-col bg-gray-0">
       <header className="flex w-full items-center justify-center py-10">
         <span className="font-extralight text-2xl text-gray-400">
-          <strong className="font-black">W</strong>A
+          <strong className="font-bold">W</strong>A
         </span>
       </header>
 
       <nav className="flex-1">
         <a
           href="/"
-          className="flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400"
+          className="group flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400 transition duration-200 hover:text-brand-500"
         >
-          <Home className="size-6 text-gray-400" />
+          <Home className="group size-6 text-gray-400 transition duration-200 group-hover:text-brand-500" />
           <span className="font-medium text-sm">Home</span>
-          <div className="h-px w-3 bg-gray-0" />
+          <div className="box-content w-3 rounded-full border border-gray-0 transition duration-200 group-hover:border-brand-500" />
         </a>
 
         <a
           href="/"
-          className="flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400"
+          className="group flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400 transition duration-200 hover:text-brand-500"
         >
-          <ReceiptText className="size-6 text-gray-400" />
+          <ReceiptText className="group size-6 text-gray-400 transition duration-200 group-hover:text-brand-500" />
           <span className="font-medium text-sm">Histórico</span>
-          <div className="h-px w-3 bg-gray-0" />
+          <div className="box-content w-3 rounded-full border border-gray-0 transition duration-200 group-hover:border-brand-500" />
         </a>
 
         <a
           href="/"
-          className="flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400"
+          className="group flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400 transition duration-200 hover:text-brand-500"
         >
-          <NotebookText className="size-6 text-gray-400" />
+          <NotebookText className="group size-6 text-gray-400 transition duration-200 group-hover:text-brand-500" />
           <span className="font-medium text-sm">Cardápio</span>
-          <div className="h-px w-3 bg-gray-0" />
+          <div className="box-content w-3 rounded-full border border-gray-0 transition duration-200 group-hover:border-brand-500" />
         </a>
 
         <a
           href="/"
-          className="flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400"
+          className="group flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400 transition duration-200 hover:text-brand-500"
         >
-          <Users className="size-6 text-gray-400" />
+          <Users className="group size-6 text-gray-400 transition duration-200 group-hover:text-brand-500" />
           <span className="font-medium text-sm">Usuários</span>
-          <div className="h-px w-3 bg-gray-0" />
+          <div className="box-content w-3 rounded-full border border-gray-0 transition duration-200 group-hover:border-brand-500" />
         </a>
       </nav>
 
       <footer>
-        <button
-          type="button"
-          className="flex w-full flex-col items-center justify-center gap-2 py-8"
+        <a
+          href="/"
+          className="group flex w-full flex-col items-center justify-center gap-2 py-6 text-gray-400 transition duration-200 hover:text-brand-500"
         >
-          <CircleUser className="size-6 text-gray-400" />
+          <CircleUser className="group size-6 text-gray-400 transition duration-200 group-hover:text-brand-500" />
           <span className="font-medium text-sm">Meu Perfil</span>
-        </button>
+          <div className="box-content w-3 rounded-full border border-gray-0 transition duration-200 group-hover:border-brand-500" />
+        </a>
 
         <button
           type="button"

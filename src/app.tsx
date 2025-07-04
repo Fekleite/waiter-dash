ee9ec6ff1449d30c4ebbf6b49ef178e45f019d43
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-03 23:04:45 -0300

    feat: create container component

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
+import { Container } from './components/container';
+import { Header } from './components/header';
 import { MenuSidebar } from './components/menu';
 
 export function App() {
   return (
     <div>
       <MenuSidebar />
+
+      <Container>
+        <Header
+          title="Home"
+          subtitle="Acompanhe os pedidos dos clientes"
+          icon="home"
+        />
+
+        <main>Welcome to the Waiter Dashboard!</main>
+      </Container>
     </div>
   );
 }
---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export function Container({ children }: Readonly<ContainerProps>) {
+  return (
+    <div className="mt-10 ml-28 flex flex-col gap-10 px-9">{children}</div>
+  );
+}

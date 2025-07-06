393250260ade05785b4d717129277c8d18376331
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-05 23:14:03 -0300

    feat: create util to format currency

--- src/components/order/card.tsx
+++ src/components/order/card.tsx
@@ -1,4 +1,5 @@
 import type { Product } from '@/types/orders';
+import { formatToCurrency } from '@/utils/formatters';
 
 interface CardProps {
   product: Product;
@@ -14,15 +15,21 @@ export function Card({ product, quantity }: Readonly<CardProps>) {
         <img
           src={`${defaultImageHost}/${product.imagePath}`}
           alt={product.name}
+          className="h-full w-full object-cover"
+          width={48}
+          height={40}
+          loading="lazy"
         />
       </div>
 
-      <div className="flex items-start gap-2">
+      <div className="flex items-start gap-3">
         <small className="text-gray-300 text-sm">{quantity}x</small>
 
         <div className="flex flex-col gap-2">
           <strong className="font-semibold">{product.name}</strong>
-          <span className="text-gray-400 text-sm">{product.price}</span>
+          <span className="text-gray-400 text-sm">
+            {formatToCurrency(product.price)}
+          </span>
         </div>
       </div>
     </li>
--- src/components/order/details.tsx
+++ src/components/order/details.tsx
@@ -1,5 +1,6 @@
+import { useMemo } from 'react';
 import type { Order } from '@/types/orders';
-
+import { formatToCurrency } from '@/utils/formatters';
 import { Card } from './card';
 
 interface DetailsProps {
@@ -7,6 +8,12 @@ interface DetailsProps {
 }
 
 export function Details({ selectedOrder }: Readonly<DetailsProps>) {
+  const orderTotal = useMemo(() => {
+    return selectedOrder.products.reduce((total, item) => {
+      return total + item.product.price * item.quantity;
+    }, 0);
+  }, [selectedOrder]);
+
   return (
     <div className="flex flex-col gap-6">
       <span className="font-medium text-sm opacity-80">Itens</span>
@@ -24,7 +31,9 @@ export function Details({ selectedOrder }: Readonly<DetailsProps>) {
       <div className="flex items-center justify-between">
         <span className="font-medium text-sm opacity-80">Total</span>
 
-        <strong className="font-semibold">R$ 120,00</strong>
+        <strong className="font-semibold">
+          {formatToCurrency(orderTotal)}
+        </strong>
       </div>
     </div>
   );

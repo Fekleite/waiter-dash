eefcb4df13ca521b24301052edf6ed355d525ca2
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-07-24 12:12:54 -0300

    feat: add toast to give user feedback

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router';
-
+import { ToastContainer } from 'react-toastify';
 import { Router } from './router';
 
 export function App() {
   return (
     <BrowserRouter>
       <Router />
+
+      <ToastContainer position="bottom-center" />
     </BrowserRouter>
   );
 }
